139522f49cb681d4a8a0215d4e222e46f3f295a9
Spencer Olson <spence1818@gmail.com>
2017-03-14 15:02:24 -0500

    add Gemfile and modify absolute paths

--- switch.rb
+++ switch.rb
@@ -10,7 +10,6 @@ stores_with_grey = JSON.parse(Net::HTTP.get(URI(grey_url)))["stores"]
 
 red_blue_url = "https://api.bestbuy.com/v1/stores(area(41.936645,-87.706683,25))+products(sku=#{red_blue_joycon_sku})?format=json&show=storeId,storeType,address,city,region,name,phone,products.name,products.sku,products&pageSize=10&apiKey=#{BEST_BUY_API_KEY}"
 stores_with_red_blue = JSON.parse(Net::HTTP.get(URI(red_blue_url)))["stores"]
-
 text_message = ""
 if stores_with_grey.any?
   stores_with_grey.each do |store|
@@ -25,7 +24,7 @@ elsif stores_with_red_blue.any?
 end
 
 # Texting
-have_not_yet_texted = File.size("/Users/solson/switch-tracker/switch-log.txt") == 0
+have_not_yet_texted = File.size("/home/solson/switch-tracker/switch-log.txt") == 0
 if have_not_yet_texted && (stores_with_grey.any? || stores_with_red_blue.any?)
   puts "SENT TEXT AT at #{Time.now}. STORES WITH GREY? #{stores_with_grey.any?} STORES WITH RED/BLUE? #{stores_with_red_blue.any?}"
   client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
