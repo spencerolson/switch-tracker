1e84a64c9a8a559b9ed1381bc03d82e42c5c3e53
Spencer Olson <solson@instructure.com>
2017-03-14 12:09:45 -0500

    use best buy API

--- switch.rb
+++ switch.rb
@@ -1,42 +1,33 @@
-require 'nokogiri'
-require 'open-uri'
+require 'net/http'
+require 'json'
 require 'twilio-ruby'
 
-doc = Nokogiri::HTML(open("https://www.zoolert.com/bestbuy-inventory-checker/?r=2%7CUS%7CBB%7C60618%7C25%7C5670003"))
-nodes = doc.xpath('//b[contains(text(), "Available Qty")]')
-result = nodes.map do |node|
-  {
-    qty: node.next_sibling.text.strip.to_i,
-    tel: node.previous.previous.previous.previous.text,
-    loc: node.previous.previous.previous.previous.previous.previous.previous.previous.text,
-    price: "$#{node.next_sibling.next_sibling.next_sibling.text.strip}"
-  }
-end
+BEST_BUY_API_KEY = ENV['BEST_BUY_KEY']
+grey_joycon_sku = "5670003"
+red_blue_joycon_sku = "5670100"
+grey_url = "https://api.bestbuy.com/v1/stores(area(41.936645,-87.706683,25))+products(sku=#{grey_joycon_sku})?format=json&show=storeId,storeType,address,city,region,name,phone,products.name,products.sku,products&pageSize=10&apiKey=#{BEST_BUY_API_KEY}"
+stores_with_grey = JSON.parse(Net::HTTP.get(URI(grey_url)))["stores"]
 
-in_stock = result.select { |option| option[:qty] > 0 }
+red_blue_url = "https://api.bestbuy.com/v1/stores(area(41.936645,-87.706683,25))+products(sku=#{red_blue_joycon_sku})?format=json&show=storeId,storeType,address,city,region,name,phone,products.name,products.sku,products&pageSize=10&apiKey=#{BEST_BUY_API_KEY}"
+stores_with_red_blue = JSON.parse(Net::HTTP.get(URI(red_blue_url)))["stores"]
 
 text_message = ""
-if in_stock.any?
-  text_message += "-- Spencer's Switch Finder --\n"
-  in_stock.each do |available|
-    if available[:qty] == 1
-      text_message += "1 Switch is available at:\n"
-    else
-      text_message += "#{available[:qty]} Switches are available at:\n"
-    end
-    text_message += "  #{available[:loc]}\n"
-    text_message += "  telephone number: #{available[:tel]}\n"
-    text_message += "  price: #{available[:price]}\n\n"
+if stores_with_grey.any?
+  stores_with_grey.each do |store|
+    address = "#{store["address"]}, #{store["city"]}, #{store["region"]}"
+    text_message += "Grey Switch at #{address} #{store["phone"]}\n"
   end
-
-else
-  text_message += "None available. Don't A Gimme A Dat"
+elsif stores_with_red_blue.any?
+  stores_with_red_blue.each do |store|
+     address = "#{store["address"]}, #{store["city"]}, #{store["region"]}"
+     text_message += "Red/Blue Switch at #{address} #{store["phone"]}\n"
+  end
 end
 
 # Texting
-have_not_yet_texted = File.size("/Users/spencer/programming/switch-runner/switch-log.txt") == 0
-if have_not_yet_texted && in_stock.any?
-  puts "SENT TEXT AT at #{Time.now}"
+have_not_yet_texted = File.size("/Users/solson/switch-tracker/switch-log.txt") == 0
+if have_not_yet_texted && (stores_with_grey.any? || stores_with_red_blue.any?)
+  puts "SENT TEXT AT at #{Time.now}. STORES WITH GREY? #{stores_with_grey.any?} STORES WITH RED/BLUE? #{stores_with_red_blue.any?}"
   client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
   twilio_number = ENV['TWILIO_NUMBER']
   telephone_numbers = [ENV['MY_NUMBER'], ENV['A_FRIENDS_NUMBER']]
@@ -45,7 +36,7 @@ if have_not_yet_texted && in_stock.any?
     client.messages.create(
       from: twilio_number,
       to: recipient,
-      body: text_message[0..600]
+      body: text_message[0..1000]
     )
   end
 end
